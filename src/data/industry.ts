8be4080ec20d5f55af154b3d4d57a63e5c15a0b7
tonogw <85993102+tonogw@users.noreply.github.com>
2026-05-26 06:44:58 +0700

    feat: completed the data/industry.ts

--- src/data/industry.ts
+++ src/data/industry.ts
@@ -1,4 +1,7 @@
-import { IndustryItem } from "../types";
+import type { IndustryItem } from "../types";
+import card from "../assets/industry/card.png";
+import hand from "../assets/industry/handed.png";
+import health from "../assets/industry/pills.png";
 
 export const IndustryHeader = {
   title: "Built for your Industry",
@@ -8,23 +11,26 @@ export const IndustryHeader = {
 
 export const industryItems: IndustryItem[] = [
   {
-    id: "",
-    title: "",
-    description: "",
-    image: "",
+    id: 1,
+    title: "Fintech",
+    description:
+      "We build secure, scalable, and compliant fintech solutions - from digital wallets to core banking systems - tailored to modern financial needs.",
+    image: card,
   },
 
   {
-    id: "",
-    title: "",
-    description: "",
-    image: "",
+    id: 2,
+    title: "E-Commerce",
+    description:
+      "Boost your online sales with fast, reliable platforms designed for seamless shopping experiences, inventory management, and payment integration.",
+    image: hand,
   },
 
   {
-    id: "",
-    title: "",
-    description: "",
-    image: "",
+    id: 3,
+    title: "Healthcare",
+    description:
+      "Empowering healthcare providers with digital solutions that improve patient care, ensure data privacy, and streamline operational workflows.",
+    image: health,
   },
 ];
